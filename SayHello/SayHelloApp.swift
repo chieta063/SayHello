12b8db1815034cd80054ee3358cf36e34439f06d
Hiroaki Abe <chieta063@gmail.com>
2023-08-01 11:40:42 +0900

    Initial Commit

---
+++ SayHello/SayHelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  SayHelloApp.swift
+//  SayHello
+//
+//  Created by 阿部 紘明 on 2023/08/01.
+//
+
+import SwiftUI
+
+@main
+struct SayHelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
